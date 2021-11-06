209e8091d0d9ea923b85d1b4c4d8d163e6d85521
balaton.tamas6 <balaton.tamas6@gmail.com>
2021-11-06 17:49:55 +0100

    getLongestSequence method refractored

--- CoinToss/src/main/java/com/tom/cointoss/Main.java
+++ CoinToss/src/main/java/com/tom/cointoss/Main.java
@@ -7,10 +7,14 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-
+    private static final int DEFAULT_QUANTITY = 0;
+    private static final int DEFAULT_LENGTH = 0;
+    
     public static void main(String[] args) {
         try {
             if (args.length != 2) {
@@ -20,28 +24,74 @@ public class Main {
             int[] longestSequence = getLongestSequence(sequenceOfCoinToss);
 
             writeToFile(args[1], prepareResult(longestSequence));
-            
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static int[] getLongestSequence(String[] sequence) {
-        int maxHeadLength = 0, maxTailLength = 0;
-        int currentHeadLength = 0, currentTailLength = 0;
+    public static int[] getLongestSequence(final String[] sequence) {
+        int currentHeadLength = DEFAULT_LENGTH, currentTailLength = DEFAULT_LENGTH;
+        
+        // Key: length of sequence, Value: quantity
+        Map<Integer, Integer> headSequences = new HashMap<>();
+        Map<Integer, Integer> tailSequences = new HashMap<>();
+
         for (String character : sequence) {
+            // HEAD
             if ("F".equals(character)) {
-                maxTailLength = Math.max(currentTailLength, maxTailLength);
-                currentTailLength = 0;
+                
+                // End of Tail sequence, put length and quantity into tailSequence map
+                if (currentTailLength != DEFAULT_LENGTH) {
+                    if (tailSequences.containsKey(currentTailLength)) {
+                        int quantity = tailSequences.get(currentTailLength) + 1;
+                        tailSequences.put(currentTailLength, quantity);
+                    } else {
+                        tailSequences.put(currentTailLength, 1);
+                    }
+                    // reset tail sequence's length
+                    currentTailLength = DEFAULT_LENGTH;
+                }
                 currentHeadLength++;
-            } 
-            if("I".equals(character)) {
-                maxHeadLength = Math.max(currentHeadLength, maxHeadLength);
-                currentHeadLength = 0;
+            }
+            // TAIL
+            if ("I".equals(character)) {
+                // End of Head sequence, put length and quantity into headSequence map
+                if (currentHeadLength != DEFAULT_LENGTH) {
+                    
+                    // If sequence already exists increment its quantity.
+                    if (headSequences.containsKey(currentHeadLength)) {
+                        int quantity = headSequences.get(currentHeadLength) + 1;
+                        headSequences.put(currentHeadLength, quantity);
+                    } else {
+                        headSequences.put(currentHeadLength, 1);
+                    }
+                    // reset Head sequence's length
+                    currentHeadLength = DEFAULT_LENGTH;
+                }
                 currentTailLength++;
             }
         }
-        return new int[]{maxHeadLength, maxTailLength};
+        // IFFIIIFFIFIII
+        return new int[]{getMaxLength(headSequences), getMaxLength(tailSequences)};
+    }
+
+    private static int getMaxLength(Map<Integer, Integer> map) {
+        int maxQuantity = DEFAULT_QUANTITY;
+        int maxLength = DEFAULT_LENGTH;
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int length = entry.getKey();
+            int quantity = entry.getValue();
+            if (maxQuantity < quantity) {
+                maxQuantity = quantity;
+                maxLength = length;
+            }
+            if (maxLength == quantity) {
+                maxLength = Math.max(maxLength, length);
+            }
+        }
+        return maxLength;
     }
 
     private static String prepareResult(int[] results) {
@@ -55,7 +105,7 @@ public class Main {
     public static String[] readLinesFromFile(String pathString) throws IOException {
         try (Scanner sc = new Scanner(new File(pathString))) {
             StringBuilder sb = new StringBuilder();
-            while (sc.hasNext()) {                
+            while (sc.hasNext()) {
                 String line = sc.nextLine();
                 sb.append(line);
             }
@@ -69,7 +119,7 @@ public class Main {
     public static void writeToFile(String pathString, String content) throws IOException {
         Path path = Paths.get(pathString);
         try (BufferedWriter writer = Files.newBufferedWriter(path, Charset.forName("UTF-8"))) {
-            writer.write(content + "\n");
+            writer.write(content);
             writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
